b2fd0e0fe3f514943d87fdad851583dd2d911ebf
xier <xier.kokis@gmail.com>
2018-04-10 13:41:03 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Button } from 'antd';
+// import './App.css';
+// const logo = require('./logo.svg');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
